bb759f75643a20953a8bf4c507fdaf285d1dfca2
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2025-02-14 17:30:05 -0500

    Adding requirements.txt

--- PySpice_Learning_Examples.py
+++ PySpice_Learning_Examples.py
@@ -14,7 +14,7 @@ circuit.R(2, 'out', circuit.gnd, 1@u_kOhm)
 print("The Circuit/Netlist: \n",circuit)
 simulator = circuit.simulator(temperature=25, nominal_temperature=25)
 print("The simulator: \n",simulator)
-anslysis = simulator.operating_point()
+analysis = simulator.operating_point()
 print('_' * 66 + "\n")
 
 # Diodes, Raw Spice and Subcircuits
@@ -28,25 +28,25 @@ circuit.raw_spice = 'D2 23 MyDiode IS=1n' + os.linesep
 
 class MySubCir(SubCircuit):
     __nodes__ = ('t_in','t_out')
-    def __init__(self, name, resester = 1@u_kOhm):
+    def __init__(self, name, resister = 1@u_kOhm):
         SubCircuit.__init__(self,name,*self.__nodes__)
-        self.R(1, 't_in', 't_out', resester)
+        self.R(1, 't_in', 't_out', resister)
         self.Diode(1,'t_in','t_out', model='MyDiode')
 
-circuit.subcircuit(MySubCir('sub1',resester = 1@u_kOhm))
+circuit.subcircuit(MySubCir('sub1',resister = 1@u_kOhm))
 circuit.X(1, 'sub1', 3, circuit.gnd)
 print("The circuit/netList:\n",circuit)
 simulator = circuit.simulator(temperature=25, nominal_temperature=25)
 print("The simulator: \n",simulator)
 print('_' * 66 + "\n")
 
-# basic and costom DC Sweep
-circuit = Circuit("basic and costom DC Sweep")
+# basic and custom DC Sweep
+circuit = Circuit("basic and custom DC Sweep")
 circuit.model('MyDiode','D',IS=4.352@u_nA, RS=0.6458@u_Ohm, BV=110@u_V, IBV=0.0001@u_V, N=1.906)
 circuit.V('input',1,circuit.gnd,10@u_V)
 circuit.Diode(1, 1, 2, model='MyDiode')
 circuit.R(1, 2, circuit.gnd, 1@u_kOhm)
 simulator = circuit.simulator(temperature=25, nominal_temperature=25)
-anslysis = simulator.operating_point()
-print('Node:',str(anslysis['1']), 'values:',np.array(anslysis['1']))
-print('Node:',str(anslysis['2']), 'values:',np.array(anslysis['2']))
+analysis = simulator.operating_point()
+print('Node:',str(analysis['1']), 'values:',np.array(analysis['1']))
+print('Node:',str(analysis['2']), 'values:',np.array(analysis['2']))
